6a08270b3f3d7d47f2293d01b12210f39acbd1af
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-09-17 14:47:14 -0500

    Terminados modelos y relaciones

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from artwork.models import Artwork
+from user.models import User
 
 # Create your models here.
+class Comment (models.Model):
+
+    content = models.CharField(max_length=200)
+    date = models.DateField()
+
+    #Relacion con Artwork
+    artwork = models.ForeignKey(
+        Artwork,
+        on_delete=models.CASCADE,
+        default=None
+    )
+
+    #Relacion con Usuario
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        default=None
+    )
--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from artwork.models import Artwork
 
 # Create your models here.
+class User(models.Model):
+
+    name = models.CharField(max_length=50, default=None)
+    userName = models.CharField(max_length=50, default=None)
+    email = models.EmailField(max_length=254, default=None)
+    password = models.CharField(max_length=50, default=None)
+
+    #Relacion con artwork
+    likedArtowks = models.ManyToManyField(Artwork, related_name='usersLiked')
